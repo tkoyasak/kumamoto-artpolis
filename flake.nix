0e30b632b411446e6f15482f7b0d46fc13e86d73
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2026-07-19 09:31:37 +0900

    Add a CI workflow running check, unit tests, and e2e (#62)
    * Add a CI workflow running check, unit tests, and e2e
    
    * Add pinact and zizmor to the devShell
    
    * Run zizmor as a pre-commit hook
    
    * Let Dependabot update GitHub Actions

--- flake.nix
+++ flake.nix
@@ -48,6 +48,11 @@
               entry = "${pkgs.gitleaks}/bin/gitleaks git --pre-commit --redact --staged";
               pass_filenames = false;
             };
+
+            zizmor = {
+              enable = true;
+              args = [ "--offline" ];
+            };
           };
 
           devShells.default = pkgs.mkShellNoCC {
@@ -59,7 +64,9 @@
               # nodejs
               oxfmt
               oxlint
+              pinact
               typescript-go
+              zizmor
             ];
           };
         };
